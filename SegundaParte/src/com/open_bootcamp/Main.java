ec18d2fbba5aa06b9b0a386b3b9a4e0769cfc9a8
fafomax <leonardodavidmoravargas@gmail.com>
2022-09-29 21:04:14 -0300

    Terminado

---
+++ SegundaParte/src/com/open_bootcamp/Main.java
@@ -0,0 +1,18 @@
+package com.open_bootcamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+       Coche miCoche = new Coche();
+       miCoche.AgregarPuerta();
+       System.out.println(miCoche.puertas);
+    }
+
+}
+class Coche {
+    public int puertas = 4;
+
+    public void AgregarPuerta() {
+        this.puertas++;
+    }
+}
